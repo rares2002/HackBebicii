7cbf71bd6c75411344a2c3a40cb4f263a8d491c9
MichaelXcX <ciuciuletemihaicm@gmail.com>
2023-04-08 11:54:37 +0300

    [update] React with typescript setup

---
+++ fe/web-react/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Luam banii</h1>
+      <p>Toti banii</p>
+    </div>
+  );
+}
+
+export default App;
