d766e5a3b5a89c4ec39fe61820b7b3659ec7484c
rares_dd <34868774+rares2002@users.noreply.github.com>
2023-04-10 10:39:10 +0300

    removed config

--- backend/src/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBoFKsmIMpheUOBa47xYa1R1AD8PlfDcY0",
-    authDomain: "fix-your-budget.firebaseapp.com",
-    projectId: "fix-your-budget",
-    storageBucket: "fix-your-budget.appspot.com",
-    messagingSenderId: "960942223175",
-    appId: "1:960942223175:web:944591da0c24d910bb2662",
-    measurementId: "G-3W68568ZF1"
-  };
